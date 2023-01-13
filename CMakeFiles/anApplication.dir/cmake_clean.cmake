56c3c5b852097e29a39027d9412380a9eaedd923
dev-shark <viacheslav.sharkovskii@ya.se>
2023-01-13 09:38:29 +0100

    Initial commit
    Add source files for my project

---
+++ CMakeFiles/anApplication.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/anApplication.dir/SecretClass.cpp.o"
+  "CMakeFiles/anApplication.dir/main.cpp.o"
+  "anApplication"
+  "anApplication.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/anApplication.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
